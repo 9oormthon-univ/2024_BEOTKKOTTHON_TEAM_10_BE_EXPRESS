d29c2bb51bcbc3da5a4178c8d80fab1f48ffd86d
jjang6251 <jjang6251@gmail.com>
2024-03-20 07:22:09 +0000

    프록시 설정

--- server.js
+++ server.js
@@ -27,6 +27,16 @@ app.get('/user/login/check', proxyController.verifyToken, userController.checkLo
 // -------------------Proxy------------------------------//
 app.use('/hi', proxyController.verifyToken, proxyController.proxy('/hi')); //proxy 예시
 
+app.use('/scholarship/all', proxyController.verifyToken, proxyController.proxy('/scholarship/all'));
+app.use('/scholarship/all/new', proxyController.verifyToken, proxyController.proxy('/scholarship/all/new'));
+app.use('/scholarship/user', proxyController.verifyToken, proxyController.proxy('/scholarship/user'));
+app.use('/scholarship/user/mount', proxyController.verifyToken, proxyController.proxy('/scholarship/user/mount'));
+app.use('/scholarship/each/status', proxyController.verifyToken, proxyController.proxy('/scholarship/each/status'));
+app.use('/scholarship/save', proxyController.verifyToken, proxyController.proxy('/scholarship/save'));
+app.use('/document', proxyController.verifyToken, proxyController.proxy('/document'));
+app.use('/document/each', proxyController.verifyToken, proxyController.proxy('/document/each'));
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
